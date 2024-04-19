511dc9ec6d30070c93c29d794673120bc81802f5
Steve at ITFS <122524858+itfs-steve@users.noreply.github.com>
2024-04-19 18:03:58 +0100

    Add Installer and Updater

--- installandupdate.ps1
+++ installandupdate.ps1
@@ -6,104 +6,119 @@ param(
 $timestamp = Get-Date -Format "yyyy-MM-dd HH-mm"
 Start-Transcript -Path "C:\CF-DDNS\Install and Update\cf-ddns-updater $timestamp log.txt"
 
-$serviceInstall = 1
+# Github Service Defaults Retrieval
+$serviceDefaults = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/CMS-29/cloudflare-ddns-agent/main/default_service_config.json"
 
-if ($NoService) {
-    Write-Host "[INFO] NoService has been specified, services will not be touched." -ForegroundColor Cyan
-    $serviceInstall = 0
-    if (Get-Service -Name "CF-DDNS" -ErrorAction SilentlyContinue) {
-        if ((Get-Service -Name "CF-DDNS").Status -eq "Running") {
-            if ($Silent) {
-                Write-Host "[WARN] A pre-existing service was found and is running, it's a good idea to stop this before proceeding." -ForegroundColor Yellow
-                Write-Host "[INFO] Silent tag used, will proceed automatically"
+if ($serviceDefaults) {
+
+    $serviceName = $serviceDefaults.name
+    $serviceDisplayName = $serviceDefaults.displayname
+    $serviceDescription = $serviceDefaults.description
+
+    $serviceInstall = 1
+
+    if ($NoService) {
+        Write-Host "[INFO] NoService has been specified, services will not be touched." -ForegroundColor Cyan
+        $serviceInstall = 0
+        if (Get-Service -Name "$serviceName" -ErrorAction SilentlyContinue) {
+            if ((Get-Service -Name "$serviceName").Status -eq "Running") {
+                if ($Silent) {
+                    Write-Host "[WARN] A pre-existing service was found and is running, it's a good idea to stop this before proceeding." -ForegroundColor Yellow
+                    Write-Host "[INFO] Silent tag used, will proceed automatically"
+                }
+                else {
+                    Write-Warning -Message "A pre-existing service was found and is running, it's a good idea to stop this before proceeding." -WarningAction Inquire
+                }
             }
-            else {
-                Write-Warning -Message "A pre-existing service was found and is running, it's a good idea to stop this before proceeding." -WarningAction Inquire
-            }
-        }
-    }
-}
-else {
-    Write-Host "[INFO] Will also install new service." -ForegroundColor Cyan
-}
-
-# Installation Location
-$installLocation = "C:\CF-DDNS\"
-
-# Service Check
-if ($serviceInstall -eq 1) {
-    if (Get-Service -Name "CF-DDNS" -ErrorAction SilentlyContinue) {
-        Write-Host "[INFO] Service CF-DDNS Exists already, will stop and remove" -ForegroundColor Cyan
-        if ((Get-Service -Name "CF-DDNS").Status -eq "Running") {
-            Stop-Service -Name "CF-DDNS" -Force
-        }
-        if (Get-Command "nssm.exe" -ErrorAction SilentlyContinue) {
-            nssm.exe remove CF-DDNS confirm
         }
     }
     else {
-        Write-Host "[INFO] Service CF-DDNS does not exist, will proceed" -ForegroundColor Cyan
+        Write-Host "[INFO] Will also install new service." -ForegroundColor Cyan
     }
-}
 
-# Github Retrieval
-$githubrepo = "https://github.com/CMS-29/cloudflare-ddns-agent"
-Write-Host "[INFO] Downloading latest release from Github"  -ForegroundColor Cyan
-Invoke-RestMethod -Uri "$githubRepo/releases/latest/download/cf-ddns.exe" -OutFile "$installLocation\cf-ddns.exe"
+    # Installation Location
+    $installLocation = "C:\CF-DDNS\"
 
-if ($serviceInstall -eq 1) {
-
-    $serviceInstallState = 0
-
-    # Check NSSM Status
-    if (Get-Command "nssm.exe" -ErrorAction SilentlyContinue) {
-        # NSSM Installed, proceed
-        Write-Host "[SUCCESS] NSSM was found" -ForegroundColor Green
-        $serviceInstallState = 1
-    }
-    else {
-        # NSSM not installed, try to install
-        Write-Host "[WARN] NSSM was not found"  -ForegroundColor Yellow
-        Write-Host "[INFO] Attempting Installation of NSSM"  -ForegroundColor Cyan
-
-        Write-Host "[INFO] Checking Winget Status" -ForegroundColor Cyan
-        if (Get-Command "winget.exe" -ErrorAction SilentlyContinue) {
-            # Winget Install
-            Write-Host "[INFO] Winget was found, will use to install" -ForegroundColor Green
-            winget install NSSM.NSSM --silent --accept-package-agreements --accept-source-agreements
+    # Service Check
+    if ($serviceInstall -eq 1) {
+        if (Get-Service -Name "$serviceName" -ErrorAction SilentlyContinue) {
+            Write-Host "[INFO] Service CF-DDNS Exists already, will stop and remove" -ForegroundColor Cyan
+            if ((Get-Service -Name "$serviceName").Status -eq "Running") {
+                Stop-Service -Name "$serviceName" -Force
+            }
             if (Get-Command "nssm.exe" -ErrorAction SilentlyContinue) {
-                $serviceInstallState = 1
+                nssm.exe remove CF-DDNS confirm
             }
         }
         else {
-            Write-Host "[WARN] Winget not installed, will try Chocolatey"  -ForegroundColor Yellow
-            Write-Host "[INFO] Checking Chocolatey Status" -ForegroundColor Cyan
-            $chocopath = "C:\ProgramData\chocolatey"
-            $chocoapp = "$chocopath\choco.exe"
-            if (Test-Path -Path $chocoapp -PathType Leaf) {
-                # Chocolately Install
-                Write-Error "[INFO] Chocolatey was found, will use to install" -ForegroundColor Green
-                Set-Location $chocopath
-                .\choco.exe install nssm -y --force
+            Write-Host "[INFO] Service CF-DDNS does not exist, will proceed" -ForegroundColor Cyan
+        }
+    }
+
+    # Github Agent Retrieval
+    $githubrepo = "https://github.com/CMS-29/cloudflare-ddns-agent"
+    Write-Host "[INFO] Downloading latest release from Github"  -ForegroundColor Cyan
+    Invoke-RestMethod -Uri "$githubRepo/releases/latest/download/cf-ddns.exe" -OutFile "$installLocation\cf-ddns.exe"
+
+    if ($serviceInstall -eq 1) {
+
+        $serviceInstallState = 0
+
+        # Check NSSM Status
+        if (Get-Command "nssm.exe" -ErrorAction SilentlyContinue) {
+            # NSSM Installed, proceed
+            Write-Host "[SUCCESS] NSSM was found" -ForegroundColor Green
+            $serviceInstallState = 1
+        }
+        else {
+            # NSSM not installed, try to install
+            Write-Host "[WARN] NSSM was not found"  -ForegroundColor Yellow
+            Write-Host "[INFO] Attempting Installation of NSSM"  -ForegroundColor Cyan
+
+            Write-Host "[INFO] Checking Winget Status" -ForegroundColor Cyan
+            if (Get-Command "winget.exe" -ErrorAction SilentlyContinue) {
+                # Winget Install
+                Write-Host "[INFO] Winget was found, will use to install" -ForegroundColor Green
+                winget install NSSM.NSSM --silent --accept-package-agreements --accept-source-agreements
                 if (Get-Command "nssm.exe" -ErrorAction SilentlyContinue) {
                     $serviceInstallState = 1
                 }
             }
             else {
-                # Install failed
-                Write-Host "[WARN] Chocolatey not installed"  -ForegroundColor Yellow
-                Write-Host "[FATAL] Cannot proceed with service, cannot install NSSM"  -ForegroundColor Red
-                $serviceInstallState = 0
+                Write-Host "[WARN] Winget not installed, will try Chocolatey"  -ForegroundColor Yellow
+                Write-Host "[INFO] Checking Chocolatey Status" -ForegroundColor Cyan
+                $chocopath = "C:\ProgramData\chocolatey"
+                $chocoapp = "$chocopath\choco.exe"
+                if (Test-Path -Path $chocoapp -PathType Leaf) {
+                    # Chocolately Install
+                    Write-Error "[INFO] Chocolatey was found, will use to install" -ForegroundColor Green
+                    Set-Location $chocopath
+                    .\choco.exe install nssm -y --force
+                    if (Get-Command "nssm.exe" -ErrorAction SilentlyContinue) {
+                        $serviceInstallState = 1
+                    }
+                }
+                else {
+                    # Install failed
+                    Write-Host "[WARN] Chocolatey not installed"  -ForegroundColor Yellow
+                    Write-Host "[FATAL] Cannot proceed with service, cannot install NSSM"  -ForegroundColor Red
+                    $serviceInstallState = 0
+                }
             }
         }
+
+        if ($serviceInstallState -eq 1) {
+            nssm.exe install "$serviceName" "$installLocation\cf-ddns.exe"
+            nssm.exe set "$serviceName" DisplayName "$serviceDisplayName"
+            nssm.exe set "$serviceName" Description "$serviceDescription"
+            nssm.exe start "$serviceName"
+        }
+
     }
 
-    if ($serviceInstallState -eq 1) {
-        nssm.exe install "CF-DDNS" "$installLocation\cf-ddns.exe"
-        nssm.exe set "CF-DDNS" DisplayName "Cloudflare DDNS Agent"
-        nssm.exe start "CF-DDNS"
-    }
-
+}
+else {
+    Write-Host "[FATAL] Cannot get service details"  -ForegroundColor Red
 }
 
 Write-Host "[INFO] COMPLETED" -ForegroundColor Green
